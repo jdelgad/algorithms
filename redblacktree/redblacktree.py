0f0965e97f0122f0145bda50e7199aac1ef4726c
Jacob Delgado <jacob.delgado@gmail.com>
2017-02-20 22:25:41 -0700

    Add delete operation

--- redblacktree/redblacktree.py
+++ redblacktree/redblacktree.py
@@ -3,12 +3,14 @@ BLACK = 1
 
 
 class Node(object):
+    color = BLACK
+    key = None
+    left = None
+    right = None
+    parent = None
+
     def __init__(self, key):
-        self.color = BLACK
         self.key = key
-        self.left = None
-        self.right = None
-        self.parent = None
 
 
 class RedBlackTree(object):
@@ -43,9 +45,9 @@ class RedBlackTree(object):
                 y = z.parent.parent.right
                 if y is not None and y.color == RED:
                     z.parent.color = BLACK
-                    y.color = BLACK #same as above
+                    y.color = BLACK
                     z.parent.parent.color = RED
-                    z = z.parent.parent  #noop
+                    z = z.parent.parent
                 else:
                     if z == z.parent.right:
                         z = z.parent
@@ -100,3 +102,103 @@ class RedBlackTree(object):
                 x.parent.left = y
         y.right = x
         x.parent = y
+
+    def delete(self, key):
+        z = self.find(key)
+        if z is None:
+            return
+
+        if z.left is None or z.right is None:
+            y = z
+        else:
+            y = self.__tree_successor(z)
+
+        if y.left is not None:
+            x = y.left
+        else:
+            x = y.right
+
+        if x is not None:
+            x.parent = y.parent
+        if y.parent is None:
+            self.root = x
+        else:
+            if y == y.parent.left:
+                 y.parent.left = x
+            else:
+                y.parent.right = x
+
+        if y != z:
+            z.key = y.key
+        if y.color == BLACK:
+            self.__delete_fixup(x)
+
+    def __tree_successor(self, x):
+        if x.right is not None:
+            return self.__tree_minimum(x.right)
+        y = x.parent
+        while y is not None and x == y.right:
+            x = y
+            y = y.parent
+        return y
+
+    def __tree_minimum(self, x):
+        while x.left is not None:
+            x = x.left
+        return x
+
+    def __delete_fixup(self, x):
+        while x is not None and x != self.root and x.color == BLACK:
+            if x == x.parent.right:
+                w = x.parent.right
+                if w.color == RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self.__rotate_left(x.parent)
+                    w = x.parent.right
+                if w.left.color == BLACK and w.right.color == BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.right.color == BLACK:
+                        w.left.color = BLACK
+                        w.color = RED
+                        self.__rotate_right(w)
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.right.color = BLACK
+                    self.__rotate_left(x.parent)
+                    x = self.root
+            else:
+                w = x.parent.left
+                if w.color == RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self.__rotate_right(x.parent)
+                    w = x.parent.left
+                if w.right.color == BLACK and w.left.color == BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.left.color == BLACK:
+                        w.right.color = BLACK
+                        w.color = RED
+                        self.__rotate_right(w)
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.right.color = BLACK
+                    self.__rotate_left(x.parent)
+                    x = self.root
+        if x is not None:
+            x.color = BLACK
+
+    def find(self, key):
+        node = self.root
+        while node is not None:
+            if key == node.key:
+                return node
+            if key > node.key:
+                node = node.right
+            else:
+                node = node.left
+        return node
--- redblacktree/unit_tests/test_redblacktree.py
+++ redblacktree/unit_tests/test_redblacktree.py
@@ -15,30 +15,6 @@ class TestRedBlackTree(unittest.TestCase):
         self.assertEqual(rbtree.root.right.key, 4)
         self.assertEqual(rbtree.root.right.color, RED)
 
-    def test_insert_three_nodes_rebalance_left(self):
-        rbtree = RedBlackTree()
-        rbtree.insert(Node(4))
-        rbtree.insert(Node(3))
-        rbtree.insert(Node(2))
-        self.assertEqual(rbtree.root.key, 3)
-        self.assertEqual(rbtree.root.color, BLACK)
-        self.assertEqual(rbtree.root.left.key, 2)
-        self.assertEqual(rbtree.root.right.color, RED)
-        self.assertEqual(rbtree.root.right.key, 4)
-        self.assertEqual(rbtree.root.right.color, RED)
-
-    def test_insert_three_nodes_rebalance_right(self):
-        rbtree = RedBlackTree()
-        rbtree.insert(Node(2))
-        rbtree.insert(Node(3))
-        rbtree.insert(Node(4))
-        self.assertEqual(rbtree.root.key, 3)
-        self.assertEqual(rbtree.root.color, BLACK)
-        self.assertEqual(rbtree.root.left.key, 2)
-        self.assertEqual(rbtree.root.right.color, RED)
-        self.assertEqual(rbtree.root.right.key, 4)
-        self.assertEqual(rbtree.root.right.color, RED)
-
     def test_insert_four_nodes(self):
         rbtree = RedBlackTree()
         rbtree.insert(Node(2))
@@ -182,6 +158,70 @@ class TestRedBlackTree(unittest.TestCase):
         self.assertEqual(rbtree.root.right.right.key, 8)
         self.assertEqual(rbtree.root.right.right.color, BLACK)
         # great grandchildren - right-right
+        self.assertIsNone(rbtree.root.right.right.left)
+        self.assertEqual(rbtree.root.right.right.right.key, 9)
+        self.assertEqual(rbtree.root.right.right.right.color, RED)
+
+    def test_insert_three_delete_root(self):
+        rbtree = RedBlackTree()
+        rbtree.insert(Node(3))
+        rbtree.insert(Node(2))
+        rbtree.insert(Node(4))
+        self.assertEqual(rbtree.root.key, 3)
+        self.assertEqual(rbtree.root.color, BLACK)
+        self.assertEqual(rbtree.root.left.key, 2)
+        self.assertEqual(rbtree.root.left.color, RED)
+        self.assertEqual(rbtree.root.right.key, 4)
+        self.assertEqual(rbtree.root.right.color, RED)
+
+        rbtree.delete(3)
+        self.assertEqual(rbtree.root.key, 4)
+        self.assertEqual(rbtree.root.color, BLACK)
+        self.assertIsNone(rbtree.root.right)
+        self.assertIsNone(rbtree.root.right)
+        self.assertEqual(rbtree.root.left.key, 2)
+        self.assertEqual(rbtree.root.left.color, RED)
+
+    def test_insert_ten_nodes_delete(self):
+        rbtree = RedBlackTree()
+        rbtree.insert(Node(2))
+        rbtree.insert(Node(3))
+        rbtree.insert(Node(4))
+        rbtree.insert(Node(1))
+        rbtree.insert(Node(0))
+        rbtree.insert(Node(5))
+        rbtree.insert(Node(6))
+        rbtree.insert(Node(7))
+        rbtree.insert(Node(8))
+        rbtree.insert(Node(9))
+
+        rbtree.delete(3)
+        self.assertEqual(rbtree.root.key, 5)
+        self.assertEqual(rbtree.root.color, BLACK)
+
+        # children
+        self.assertEqual(rbtree.root.left.key, 4)
+        self.assertEqual(rbtree.root.left.color, RED)
+        self.assertEqual(rbtree.root.right.key, 7)
+        self.assertEqual(rbtree.root.right.color, RED)
+
+        # grandchildren - left
+        self.assertEqual(rbtree.root.left.left.key, 1)
+        self.assertEqual(rbtree.root.left.left.color, BLACK)
+        self.assertIsNone(rbtree.root.left.right)
+        # grandchildren - left-left
+        self.assertEqual(rbtree.root.left.left.left.key, 0)
+        self.assertEqual(rbtree.root.left.left.left.color, RED)
+        self.assertEqual(rbtree.root.left.left.right.key, 2)
+        self.assertEqual(rbtree.root.left.left.right.color, RED)
+
+        # grandchildren - right
+        self.assertEqual(rbtree.root.right.left.key, 6)
+        self.assertEqual(rbtree.root.right.left.color, BLACK)
+        self.assertEqual(rbtree.root.right.right.key, 8)
+        self.assertEqual(rbtree.root.right.right.color, BLACK)
+        # great grandchildren - right-right
+        self.assertIsNone(rbtree.root.right.right.left)
         self.assertEqual(rbtree.root.right.right.right.key, 9)
         self.assertEqual(rbtree.root.right.right.right.color, RED)
 
